7558f94add99ed231901a68128d3aa425a7f5759
Sean <sean.jennings1@gmail.com>
2018-01-17 01:46:04 -0800

    Fix formatting in neural net and policy; add gitignore

--- NeuralNet.py
+++ NeuralNet.py
@@ -2,6 +2,8 @@ import numpy as np
 import copy
 import random
 
+NUM_SQUARES = 16
+
 netParams = {
     'alpha': 0.01,
     'gamma': 1,
@@ -9,51 +11,61 @@ netParams = {
     'hiddenLayers': 1,
     'hiddenLayerSizes': [40],
     'nodesPerSquare': 3,
-    'cutoffVals': [1,8,64,2048], #Values up to and including cutoffVal[i+1] are included in node i
-                        #len(cutoffVals) = nodesPerSquare+1
+    # Values up to and including cutoffVal[i+1] are included in node i
+    # len(cutoffVals) = nodesPerSquare+1
+    'cutoffVals': [1,8,64,2048],
     'boardToStateConv': 'log' #type of conversion from board to state
 }
+
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 
 class NeuralNet():
-    def __init__(self,netParams):
+    def __init__(self, netParams):
         self.params = netParams
-        self.thetas = list()
-        self.a = list() # the activation values of the hidden layers
-        self.delta = list()
+        self.thetas = []
+        self.act_vals = [] # the activation values of the hidden layers
+        self.delta = []
         
-        for i in range(self.params['hiddenLayers']+1): #theta matrices 
+        # Initialize theta matrices.
+        for i in range(self.params['hiddenLayers'] + 1):
             if i == 0:
-                size = (self.params['hiddenLayerSizes'][i],16*self.params['nodesPerSquare'])
-                self.a.append(np.zeros(self.params['hiddenLayerSizes'][i]))
-                self.delta.append(np.zeros(self.params['hiddenLayerSizes'][i]))
+                size = (self.params['hiddenLayerSizes'][i],
+                            NUM_SQUARES*self.params['nodesPerSquare'])
+                self.act_vals.append(
+                    np.zeros(self.params['hiddenLayerSizes'][0]))
+                self.delta.append(
+                    np.zeros(self.params['hiddenLayerSizes'][0]))
             elif i == self.params['hiddenLayers']: #for 
                 size = (1,self.params['hiddenLayerSizes'][i-1])
                 self.delta.append(0)
             else:
-                size = (self.params['hiddenLayerSizes'][i],self.params['hiddenLayerSizes'][i-1])
-                self.a.append(np.zeros(self.params['hiddenLayerSizes'][i]))
-                self.delta.append(np.zeros(self.params['hiddenLayerSizes'][i]))
+                size = (self.params['hiddenLayerSizes'][i],
+                        self.params['hiddenLayerSizes'][i-1])
+                self.act_vals.append(
+                    np.zeros(self.params['hiddenLayerSizes'][i]))
+                self.delta.append(
+                    np.zeros(self.params['hiddenLayerSizes'][i]))
             self.thetas.append(np.zeros(size))
 
-        
-        
-
-    def forward(self,board):
-        #forward propogation to return the value of the current position of the board
+    def forward(self, board):
+        """
+        Forward propogation to return the value of the current position of
+        the board
+        """
         state = self.getStateVector(board)
-        self.a[0] = sigmoid(self.thetas[0].dot(state))
+        self.act_vals[0] = sigmoid(self.thetas[0].dot(state))
         for i in range(self.params['hiddenLayers']-1):
             #TODO: add bias unit
-            self.a[i+1] = sigmoid(self.thetas[i+1].dot(self.a[i]))
+            self.act_vals[i+1] = sigmoid(self.thetas[i+1].dot(self.act_vals[i]))
 
-        return self.thetas[self.params['hiddenLayers']].dot(self.a[self.params['hiddenLayers']-1])
+        return self.thetas[self.params['hiddenLayers']].dot(
+                            self.act_vals[self.params['hiddenLayers']-1])
 
-    def getStateVectorLog(self,board):
-        state = np.zeros(16*self.params['nodesPerSquare'])
+    def getStateVectorLog(self, board):
+        state = np.zeros(NUM_SQUARES*self.params['nodesPerSquare'])
         for i in range(4):
             for j in range(4):
                 val = board.getTile(i,j)
@@ -61,10 +73,14 @@ class NeuralNet():
                     if val<=self.params['cutoffVals'][k]:
                         stateVal = 0
                     elif val<self.params['cutoffVals'][k+1]:
-                        stateVal = (np.log(val)-np.log(self.params['cutoffVals'][k]))/np.log(2)
-                        lowerLogVal = np.log(self.params['cutoffVals'][k])/np.log(2)
-                        upperLogVal = np.log(self.params['cutoffVals'][k+1])/np.log(2)
-                        stateVal = stateVal/(upperLogVal-lowerLogVal) #normalize values to range (0,1)
+                        stateVal = (np.log(val) -
+                            np.log(self.params['cutoffVals'][k]))/np.log(2)
+                        lowerLogVal = np.log(
+                            self.params['cutoffVals'][k])/np.log(2)
+                        upperLogVal = np.log(
+                            self.params['cutoffVals'][k+1])/np.log(2)
+                        # Normalize values to range (0,1)
+                        stateVal = stateVal/(upperLogVal-lowerLogVal)
                     else:
                         stateVal = 1
                     state[self.params['nodesPerSquare']*(4*i+j)+k] = stateVal
@@ -72,31 +88,34 @@ class NeuralNet():
 
 
 
-    def getStateVector(self,board):
+    def getStateVector(self, board):
         if self.params['boardToStateConv'] == 'log':
             return self.getStateVectorLog(board)
         else:
             raise ValueError('boardToStateConv is not valid')
 
-    def backward(self,board,estTrueVal):
-        #calculates the gradient of the value function in the direction of the minimum error between
-        # the approximated neural net value and the true value (estimated by the reward and value of the next state)
-
+    def backward(self, board, estTrueVal):
+        """
+        Calculates the gradient of the value function in the direction of
+        the minimum error betweem the approximated neural net value and the
+        true value (estimated by the reward and value of the next state)
+        """
         value = self.forward(board)
-        self.delta[self.params['hiddenLayers']] = estTrueVal - value # error of output of net
+        # Error of output of net
+        self.delta[self.params['hiddenLayers']] = estTrueVal - value
         dThetas = list() #initialize the gradient of the theta
         
-        for i in range(self.params['hiddenLayers']-1,-1,-1):
-            self.delta[i] = self.thetas[i+1].T.dot(self.delta[i+1])*self.a[i]*(1-self.a[i])
+        for i in range(self.params['hiddenLayers'] - 1, -1, -1):
+            self.delta[i] = self.thetas[i+1].T.dot(self.delta[i + 1]) * \
+            self.act_vals[i] * (1 - self.act_vals[i])
 
-
-        for i in range(self.params['hiddenLayers']+1):
+        for i in range(self.params['hiddenLayers'] + 1):
             if i == 0:
                 state = self.getStateVector(board)
-                dThetai = np.c_[self.delta[0]].dot(np.atleast_2d(state)) #first dTheta matrix uses state instead of of hidden layer
+                # First dTheta matrix uses state instead of of hidden layer
+                dThetai = np.c_[self.delta[0]].dot(np.atleast_2d(state))
             else:
-                dThetai = np.c_[self.delta[i]].dot(np.atleast_2d(self.a[i-1]))
+                dThetai = np.c_[self.delta[i]].dot(
+                                np.atleast_2d(self.act_vals[i-1]))
             dThetas.append(dThetai)
         return dThetas
-
-    
--- Policy.py
+++ Policy.py
@@ -4,13 +4,13 @@ import copy
 from random import random, choice
 
 
-
-
-
-def epGreedyPolicy(board,net,ep):
-    #given a board and a neural net, returns a move based on an ep greedy policy
-    #returns move with highest predicted value 1-ep of the time
-    #Otherwise, ep of the time, the policy explores and randomly picks one of the suboptimal values
+def epGreedyPolicy(board, net, ep):
+    """
+    Given a board and a neural net, returns a move based on an ep greedy
+    policy. Returns move with highest predicted value 1-ep of the time
+    Otherwise, ep of the time, the policy explores and randomly picks
+    one of the suboptimal values
+    """
     moves = ['up','down','right','left']
     validMoves = []
     for move in moves:
@@ -20,18 +20,20 @@ def epGreedyPolicy(board,net,ep):
     #find the values of all after states to determine best
     maxAfterValue = float('-inf')
     for move in validMoves:
-        boardCopy = Board2048.move(board,move,afterState=True)
+        boardCopy = Board2048.move(board, move, afterState=True)
         afterValue = net.forward(boardCopy)
         if afterValue>maxAfterValue:
             maxAfterValue = afterValue
             bestMove = move
 
     r = random()
-    if r>ep: #1-ep probability of best move
+    if r>ep: # 1-ep probability of best move
         return bestMove
     else:
-        if len(validMoves)==1: # if there's only one move then pick still pick the best
-            return bestMove     #there should always be at least one valid move or else game would be over
+        # If there's only one move then pick still pick the best. There
+        # should always be at least one valid move or else game would be over
+        if len(validMoves)==1:
+            return bestMove
         else:
-            validMoves.pop(validMoves.index(bestMove)) #remove best move
+            validMoves.pop(validMoves.index(bestMove))  # remove best move
             return choice(validMoves)                   # even chance of the rest
