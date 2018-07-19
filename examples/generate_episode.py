d599010e19779ea7c3a193595cea31cf98fea5e7
Sean <sean.jennings1@gmail.com>
2018-07-19 01:23:43 -0700

    Add saving data and loading episode data.

--- examples/generate_episode.py
+++ examples/generate_episode.py
@@ -1,4 +1,5 @@
 import numpy as np
+import h5py
 
 from episode_generator import EpisodeGenerator
 from Policy import RandomPolicy
@@ -7,12 +8,22 @@ from returns import sum_cumulative_returns
 episode_generator = EpisodeGenerator()
 policy = RandomPolicy()
 
-move_history = episode_generator.play_policy(policy)
-returns_array = sum_cumulative_returns(move_history)
+
+num_episodes = 1000
+move_history = episode_generator.play_policy_multi(policy, num_episodes)
+write_to_file = True
 
 # print('Board Score: ', board.score)
-print('Reward sum: ', np.sum(move_history['reward']))
-print('Number of moves: ', move_history.shape[0])
-print('Final board position:\n', move_history[-1]['after_state'])
-print('Returns of initial position: ', returns_array[0])
-print('Returns after final position: ', returns_array[-1])
+# print('Reward sum: ', np.sum(move_history['reward']))
+# print('Number of moves: ', move_history.shape[0])
+# print('Final board position:\n', move_history[-1]['after_state'])
+# print('Returns of initial position: ', returns_array[0])
+# print('Returns after final position: ', returns_array[-1])
+# print('Returns calculated in move_history: ', move_history['return'][0],
+#       move_history['return'][-1])
+
+print(move_history.shape[0])
+if write_to_file:
+    f = h5py.File('episodes.hdf5', 'w')
+    f.create_dataset('random_policy', data=move_history)
+    f.close()
--- neural_net.py
+++ neural_net.py
@@ -1,33 +1,41 @@
 import numpy as np
+import h5py
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from episode_generator import EpisodeGenerator
 from Policy import RandomPolicy
 from state_vector import log_state_vector
-from returns import sum_cumulative_returns
 
-episode_generator = EpisodeGenerator()
-policy = RandomPolicy()
-num_trials = 100
+load_from_hdf5 = True
+
+if not load_from_hdf5:
+    episode_generator = EpisodeGenerator()
+    policy = RandomPolicy()
+    num_trials = 100
+    print('Generating Episodes...')
+    move_history = episode_generator.play_policy_multi(policy, num_trials)
+else:
+    f = h5py.File('episodes.hdf5')
+    move_history = f['random_policy']
+
+
 cutoff_values = np.array([1, 8, 32, 128, 2048])
-state_shape = (4, 4, len(cutoff_values) - 1)
-new_state_shape = (np.prod(state_shape),)
+state_shape = (16 * (len(cutoff_values) - 1),)
 
 model = Sequential([
-    Dense(32, input_shape=new_state_shape, activation='sigmoid'),
-    Dense(1),
-    Activation('linear')
+    Dense(32, input_shape=state_shape),
+    Dense(32, activation='sigmoid'),
+    Dense(1)
     ])
 
 model.compile(optimizer='rmsprop',
               loss='mse')
 
-print('Generating Episodes...')
-move_history = episode_generator.play_policy_multi(policy, num_trials)
 state_vector = log_state_vector(move_history['after_state'], cutoff_values)
-returns = sum_cumulative_returns(move_history)
+returns = move_history['return']
 print('Number of moves: ', move_history.shape[0])
 
 print('Fitting model')
 
-history = model.fit(state_vector, returns, epochs=5)
+history = model.fit(state_vector, returns, validation_split=0.7, epochs=5)
+import pdb; pdb.set_trace()
